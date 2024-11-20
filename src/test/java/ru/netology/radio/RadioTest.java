b4fd12cf3e9de273bdcfd475ff1c20cee56e63b8
Sergey_Sonne <berit2008@mail.ru>
2024-11-21 02:52:02 +0300

    Добавлены изменения

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -1,68 +1,90 @@
 package ru.netology.radio;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class RadioTest {
+public class RadioTest {
 
     @Test
-    void shouldSetStationWithinValidRange() {
+    public void testNextStation() {
         Radio radio = new Radio();
-        radio.setCurrentStation(5);
-        assertEquals(5, radio.getCurrentStation());
-    }
 
-    @Test
-    void shouldNotSetStationOutOfRange() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(10);
-        assertEquals(0, radio.getCurrentStation()); // Станция не изменилась
-    }
+        radio.setCurrentStation(8); // Станция 8
+        radio.nextStation(); // Следующая станция
+        assertEquals(9, radio.getCurrentStation()); // Ожидаем станцию 9
 
-    @Test
-    void shouldWrapToMinStationWhenNextFromMax() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(9);
-        radio.nextStation();
+        radio.nextStation(); // Следующая станция (должна вернуться на 0)
         assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
-    void shouldWrapToMaxStationWhenPrevFromMin() {
+    public void testPrevStation() {
         Radio radio = new Radio();
-        radio.setCurrentStation(0);
-        radio.prevStation();
+
+        radio.setCurrentStation(0); // Станция 0
+        radio.prevStation(); // Предыдущая станция (должна вернуться на 9)
         assertEquals(9, radio.getCurrentStation());
+
+        radio.setCurrentStation(5); // Станция 5
+        radio.prevStation(); // Предыдущая станция
+        assertEquals(4, radio.getCurrentStation());
     }
 
     @Test
-    void shouldIncreaseVolume() {
+    public void testIncreaseVolume() {
         Radio radio = new Radio();
-        radio.increaseVolume();
-        assertEquals(1, radio.getCurrentVolume());
-    }
+        radio.setCurrentVolume(50); // Громкость 50
+        radio.increaseVolume(); // Увеличиваем громкость
+        assertEquals(51, radio.getCurrentVolume());
 
-    @Test
-    void shouldNotIncreaseVolumeBeyondMax() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(100);
-        radio.increaseVolume();
+        radio.setCurrentVolume(100); // Максимальная громкость
+        radio.increaseVolume(); // Не увеличится, т.к. уже на максимуме
         assertEquals(100, radio.getCurrentVolume());
     }
 
     @Test
-    void shouldDecreaseVolume() {
+    public void testDecreaseVolume() {
         Radio radio = new Radio();
-        radio.setCurrentVolume(50);
-        radio.decreaseVolume();
+        radio.setCurrentVolume(50); // Громкость 50
+        radio.decreaseVolume(); // Уменьшаем громкость
         assertEquals(49, radio.getCurrentVolume());
+
+        radio.setCurrentVolume(0); // Минимальная громкость
+        radio.decreaseVolume(); // Не уменьшится, т.к. уже на минимуме
+        assertEquals(0, radio.getCurrentVolume());
     }
 
     @Test
-    void shouldNotDecreaseVolumeBelowMin() {
+    public void testSetCurrentStation() {
         Radio radio = new Radio();
-        radio.decreaseVolume();
-        assertEquals(0, radio.getCurrentVolume());
+
+        // Проверка, что станция устанавливается в допустимом диапазоне
+        radio.setCurrentStation(5);
+        assertEquals(5, radio.getCurrentStation());
+
+        // Проверка, что станция не устанавливается ниже минимальной
+        radio.setCurrentStation(-1);
+        assertNotEquals(-1, radio.getCurrentStation()); // Ожидаем, что значение не изменится
+
+        // Проверка, что станция не устанавливается выше максимальной
+        radio.setCurrentStation(10);
+        assertNotEquals(10, radio.getCurrentStation()); // Ожидаем, что значение не изменится
+    }
+
+    @Test
+    public void testSetCurrentVolume() {
+        Radio radio = new Radio();
+
+        // Проверка, что громкость устанавливается в допустимом диапазоне
+        radio.setCurrentVolume(50);
+        assertEquals(50, radio.getCurrentVolume());
+
+        // Проверка, что громкость не устанавливается ниже 0
+        radio.setCurrentVolume(-1);
+        assertNotEquals(-1, radio.getCurrentVolume()); // Ожидаем, что значение не изменится
+
+        // Проверка, что громкость не устанавливается выше 100
+        radio.setCurrentVolume(101);
+        assertNotEquals(101, radio.getCurrentVolume()); // Ожидаем, что значение не изменится
     }
 }
